6a1db11662cf26a0173b2481920ea65054f7a6af
Kevin Traini <kevin@ktraini.com>
2020-10-14 10:31:27 +0200

    Question 2

--- questions.cpp
+++ questions.cpp
@@ -22,3 +22,39 @@ int search(std::string search, std::string str) {
     // Si on a pas trouvé, on renvoie faux
     return -1;
 }
+
+std::map<std::string, std::vector<int> *> *search(const std::string &str, std::vector<std::string> &keyword) {
+    auto *result = new std::map<std::string, std::vector<int> *>();
+
+    auto *compts = new std::vector<int>(keyword.size());
+    for (int i = 0; i < keyword.size(); ++i) compts->push_back(0);
+
+    for (int i = 0; i < str.size(); i++) {
+        for (int j = 0; j < keyword.size(); ++j) {
+            if (keyword[j][(*compts)[j]] != str[i]) (*compts)[j] = 0;
+            if (keyword[j][(*compts)[j]] == str[i]) (*compts)[j]++;
+
+            if ((*compts)[j] == keyword[j].size()) {
+                auto it = result->find(keyword[j]);
+                if (it == result->end()) {
+                    auto *vector = new std::vector<int>();
+                    vector->push_back(i - keyword[j].size() + 1);
+                    result->emplace(keyword[j], vector);
+                } else {
+                    std::vector<int> *vector = it->second;
+                    vector->push_back(i - keyword[j].size() + 1);
+                    result->emplace(keyword[j], vector);
+                }
+            }
+        }
+    }
+
+    auto it = keyword.begin();
+    for (; it != keyword.end(); it++) {
+        if (result->find(*it) == result->end()) {
+            auto *i = new std::vector<int>();
+            result->emplace(*it, i);
+        }
+    }
+    return result;
+}
--- questions.hpp
+++ questions.hpp
@@ -2,9 +2,16 @@
 #define TP1_QUESTIONS_HPP
 
 #include <string>
+#include <cctype>
+#include <vector>
+#include <map>
+#include <sstream>
 
 // Question 1
 // Cherche search dans str
 int search(std::string search, std::string str);
+
+// Question 2
+std::map<std::string, std::vector<int> *> *search(const std::string &str, std::vector<std::string> &keyword);
 
 #endif //TP1_QUESTIONS_HPP
